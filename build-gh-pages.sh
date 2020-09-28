59e5f3402643a9de636bfa2d5ab1440156f0420e
Xuan Huang <huxpro@gmail.com>
2020-09-27 23:25:58 -0700

    Gh-pages only support building from docs

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -3,16 +3,16 @@
 SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 SRC=$SCRIPT
-GH_PAGES="$SCRIPT/gh-pages"
+PROD="$SCRIPT/docs" # used by github pages
 
 build() {
-  mkdir $GH_PAGES && \
-  cp -r "$SRC/bundleOutput" "$GH_PAGES/bundleOutput" && \
-  cp "$SRC/indexProduction.html" "$GH_PAGES/index.html"
+  mkdir $PROD && \
+  cp -r "$SRC/bundleOutput" "$PROD/bundleOutput" && \
+  cp "$SRC/indexProduction.html" "$PROD/index.html"
 }
 
 cleanup() {
-  rm -rf $GH_PAGES
+  rm -rf $PROD
 }
 
 cleanup && build
